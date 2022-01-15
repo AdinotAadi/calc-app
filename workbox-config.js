cc62d7e161248889687c66c8badbb4af5689e8cf
Aditya Patel <patel.aditya1@outlook.com>
2022-01-15 15:14:35 +0530

    Overhaul #1

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	globDirectory: './',
+	globDirectory: '.',
 	globPatterns: [
-		'**/*.{html,json,js,png,svg,css}'
+		'**/*.{css,html,js,md}'
 	],
-	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
-	]
+	],
+	swDest: 'sw.js'
 };
